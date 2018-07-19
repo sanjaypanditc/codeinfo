4a192477e27b096ef99ef88c87bebb5986383bcb
sanjay chaturvedi <sanjay.chaturvedi@dotsquares.com>
2018-07-19 11:58:46 +0530

    add theme

--- conf.py
+++ conf.py
@@ -84,6 +84,20 @@ todo_include_todos = False
 #
 #html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
